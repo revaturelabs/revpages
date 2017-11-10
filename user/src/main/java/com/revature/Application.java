d9074073f7bff2dd3fd765a6f559cfc93d03ebb2
Peter Alagna <peter.alagna@revature.com>
2017-11-10 13:56:33 -0500

    Working user service being detected by Eureka.

--- user/src/main/java/com/revature/Application.java
+++ user/src/main/java/com/revature/Application.java
@@ -1,15 +1,31 @@
 package com.revature;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+
+import com.revature.model.User;
+import com.revature.service.UserService;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+public class Application implements CommandLineRunner {
 
+	@Autowired
+	private ApplicationContext context;
+	
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		UserService service = context.getBean(UserService.class);
+		service.registerUser(new User(0,"palagnajr@gmail.com","1234","MODERATOR"));
+		service.registerUser(new User(0,"test@gmail.com","1234","USER"));
+	}
 }
 
--- user/src/main/java/com/revature/respository/UserRepository.java
+++ user/src/main/java/com/revature/respository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.revature.respository;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.model.User;
 
