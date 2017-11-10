ce0642e99131264f8ecec376eb524591897f1795
Peter Alagna <peter.alagna@revature.com>
2017-11-10 13:09:38 -0500

    Client discovery not working.

--- user/src/main/java/com/revature/Application.java
+++ user/src/main/java/com/revature/Application.java
@@ -1,29 +1,15 @@
 package com.revature;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.revature.model.User;
-import com.revature.service.UserService;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+@EnableDiscoveryClient
+public class Application {
 
-	@Autowired
-	private ApplicationContext context;
-	
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-	
-	@Override
-    public void run(String... args) throws Exception {
-		UserService userService = context.getBean(UserService.class);
-		userService.registerUser(new User(0,"palagnajr@gmail.com","1234","MODERATOR"));
-		userService.registerUser(new User(0,"TEST@gmail.com","1234","USER"));
     }
 }
 
