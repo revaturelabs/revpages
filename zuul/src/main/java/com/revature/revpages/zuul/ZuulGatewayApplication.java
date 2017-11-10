c100e5f8413dbe62e8d640182e1707ae00cab1cd
Patrick <patrick.walsh@revature.com>
2017-11-10 11:18:51 -0500

    there is no dana.. only zuul

---
+++ zuul/src/main/java/com/revature/revpages/zuul/ZuulGatewayApplication.java
@@ -0,0 +1,41 @@
+package com.revature.revpages.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Netflix Zuul - API Gateway
+ * 
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("PATCH");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+}
